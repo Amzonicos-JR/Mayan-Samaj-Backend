8ba3e8740211a5383a229f69d96ffd12ece50b53
agutierrez <agutierrez-2021188@kinal.edu.gt>
2023-07-13 23:26:34 -0600

    .

--- mayanSamaj (back)/configs/app.js
+++ mayanSamaj (back)/configs/app.js
@@ -12,14 +12,8 @@ const app = express();
 const port = process.env.PORT || 3500;
 
 // Routes
-const eventRoutes = require('../src/events_/events.routes')
-const serviceRoutes = require('../src/services_/services.routes')
-const roomRoutes = require('../src/room/room.routes')
-const userRoutes = require('../src/user/user.routes')
-const hotelRoutes = require('../src/hotel/hotel.routes')
-const reservationRoutes = require('../src/reservation/reservation.routes')
-const invoiceDetailRoutes = require('../src/invoiceDetail/invoiceDetail.routes')
-const billRoutes = require('../src/bill/bill.routes')
+// const userRoutes = require('../src/user/user.routes')
+
 
 //CONFIGURAR EL SERVIDOR HTTP DE EXPRESS
 app.use(express.urlencoded({ extended: false }));
@@ -29,14 +23,8 @@ app.use(helmet());
 app.use(morgan('dev'));
 
 // Ruta
-app.use('/event', eventRoutes);
-app.use('/service', serviceRoutes)
-app.use('/room', roomRoutes)
-app.use('/user', userRoutes)
-app.use('/hotel', hotelRoutes)
-app.use('/reservation', reservationRoutes)
-app.use('/invoicedetail', invoiceDetailRoutes)
-app.use('/bill', billRoutes)
+// app.use('/user', userRoutes)
+
 
 //Función donde se levanta el servidor
 exports.initServer = () => {
