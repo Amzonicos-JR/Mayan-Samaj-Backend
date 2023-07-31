73f0c7ff68ebecd123a880ae76006f7c18ea1976
agutierrez <agutierrez-2021188@kinal.edu.gt>
2023-07-31 00:16:31 -0600

    Finish 0.1

--- mayanSamaj (back)/src/email/email.routes.js
+++ mayanSamaj (back)/src/email/email.routes.js
@@ -5,5 +5,5 @@ const emailController = require('./email.controller')
 
 api.post('/send/:id', emailController.sendEmail)
 api.get('/get/:id', emailController.getByUser)
-
+api.get('/see/:id', emailController.seeEmail);
 module.exports = api;
